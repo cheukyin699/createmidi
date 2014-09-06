47c18a882808162783af1e3ade42e863b5ff8f5b
cheukyin699 <cheukyin699@yahoo.com>
2014-09-05 21:46:57 -0700

    Increment version number and renamed file

--- example/example.cpp
+++ example/example.cpp
@@ -1,3 +1,7 @@
+/**
+ * An example of how this library could be used
+ * @example example.cpp
+ */
 #include <MIDIfile.h>
 #include <iostream>
 #include <fstream>
@@ -7,23 +11,31 @@ using namespace std;
 
 int main()
 {
+	//! MIDIfile
 	MIDIfile f;
-	f.setFormatType(0);
-	f.setTimeDivision((unsigned short)0xe0);
 	MIDItrack t;
 	MIDItrackevt evt;
+
+	//! Set format type
+	f.setFormatType(0);
+	//! Set time division
+	f.setTimeDivision((unsigned short)0xe0);
+
+	//! Set event (keydown)
 	evt.delta_time = 0;
 	evt.evt_type_channel = 0x90;
 	evt.param1 = makeNote(C);
 	evt.param2 = 100;
 	t.addEvent(evt);
 
+	//! Set event (keyup)
 	evt.delta_time = 100;
 	evt.evt_type_channel = 0x80;
 	evt.param1 = makeNote(C);
 	evt.param2 = 100;
 	t.addEvent(evt);
 
+	//! Adds the track
 	f.addTrack(t);
 
 	ofstream ff("test.midi", ios::binary);
