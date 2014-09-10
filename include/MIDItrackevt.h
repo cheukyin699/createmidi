9bc36d2d903722f76bf4cdf967b392e33a99dc61
cheukyin699 <cheukyin699@yahoo.com>
2014-09-09 19:27:30 -0700

    Added controller types handling and some meta event constants

--- include/MIDItrackevt.h
+++ include/MIDItrackevt.h
@@ -30,6 +30,63 @@ namespace MIDI
 	const char Pitch_Bend_evt = 0xE;
 	const char Meta_evt = 0xFF;
 
+	// Controller Types
+	const char Bank_Select				= 0x0;
+	const char Modulation				= 0x1;
+	const char Breath_Controller		= 0x2;
+	const char Foot_Controller			= 0x4;
+	const char Portamento_Time			= 0x5;
+	const char Data_Entry				= 0x6;
+	const char Main_Volume				= 0x7;
+	const char Balance					= 0x8;
+	const char Pan						= 0xA;
+	const char Expression_Controller	= 0xB;
+	const char Effect_Control1			= 0xC;
+	const char Effect_Control2			= 0xD;
+	//! General-Purpose Controllers 1-4 (This one is for calculations)
+	const char Base_GPC					= 0x10;
+	//! LSB for Controllers 0-31 (This is for calculations)
+	const char Base_Controller_LSB		= 0x20;
+	//! For sustain
+	const char Damper_Pedal				= 0x40;
+	const char Portamento				= 0x41;
+	const char Sostenuto				= 0x42;
+	const char Soft_Pedal				= 0x43;
+	const char Legato_Footswitch		= 0x44;
+	const char Hold2					= 0x45;
+	//! Sound Controllers 1-10 (Used for calculations)
+	const char Base_Sound_Controller	= 0x46;
+	//! General-Purpose Controllers 5-8 (Used for calculations)
+	const char Base_Upper_GPC			= 0x50;
+	const char Portamento_Control		= 0x54;
+	//! Effects 1-5 (Used for calculations)
+	const char Base_Effects				= 0x5B;
+	const char Data_Increment			= 0x60;
+	const char Data_Decrement			= 0x61;
+	const char NR_Param_Num_LSB			= 0x62;
+	const char NR_Param_Num_MSB			= 0x63;
+	const char R_Param_Num_LSB			= 0x64;
+	const char R_Param_Num_MSB			= 0x65;
+	//! Mode Messages 1-7 (Used for calculations)
+	const char Base_Mode_Msg			= 0x79;
+
+	// Meta Event Type Numbers
+	const char Meta_Seq_Num				= 0x0;
+	const char Meta_Text_Evt			= 0x1;
+	const char Meta_Copyright_Notice	= 0x2;
+	const char Meta_Seq_Track_Name		= 0x3;
+	const char Meta_Instrument_Name		= 0x4;
+	const char Meta_Lyrics				= 0x5;
+	const char Meta_Marker				= 0x6;
+	const char Meta_Cue_Point			= 0x7;
+	const char Meta_Channel_PFX			= 0x20;
+	const char Meta_EOT					= 0xFF;
+	const char Meta_Set_Tempo			= 0x51;
+	const char Meta_SMPTE_Offset		= 0x54;
+	const char Meta_Time_Signature		= 0x58;
+	const char Meta_Key_Signature		= 0x59;
+	const char Meta_Seq_Specific		= 0x7F;
+
 	/**
 	 * An enum of some notes
 	 */
@@ -77,8 +134,51 @@ namespace MIDI
 	 * @return The MIDI note number
 	 * @see MIDInote
 	 */
-	unsigned char makeNote(MIDInote note, short octave = 0);
+	inline unsigned char makeNote(MIDInote note, short octave = 0)
+	{
+		return (unsigned char)((int)note + 60 + 12 * octave);
+	}
 
+	/**
+	 * Gets the GPC<num> using Base_GPC and Base_Upper_GPC
+	 * @param GPC_num The General-Purpose Controller number (1-8)
+	 * @return The GPC<num> type (number) or 0 if unsuccessful
+	 * @see Base_GPC
+	 * @see Base_Upper_GPC
+	 */
+	unsigned char GPCtype(unsigned short GPC_num);
+
+	/**
+	 * Gets the Controller_LSB<num> using Base_Controller_LSB
+	 * @param cLSB_num The Controller LSB number (0-31)
+	 * @return The Controller_LSB<num> type (number) or 0 if unsuccessful
+	 * @see Base_Controller_LSB
+	 */
+	unsigned char Controller_LSB_type(unsigned short cLSB_num);
+
+	/**
+	 * Gets the Sound_Controller<num> using Base_Sound_Controller
+	 * @param SC_num The Sound Controller number (1-10)
+	 * @return The Sound_Controller<num> type (number) or 0 if unsuccessful
+	 * @see Base_Sound_Controller
+	 */
+	unsigned char Sound_Controller_type(unsigned short SC_num);
+
+	/**
+	 * Gets the Effects<num> using Base_Effects
+	 * @param E_num The Effects number (1-5)
+	 * @return The Effects<num> type (number) or 0 if unsuccessful
+	 * @see Base_Effects
+	 */
+	unsigned char Effects_type(unsigned short E_num);
+
+	/**
+	 * Gets the Mode_Msg<num> using Base_Mode_Msg
+	 * @param MM_num The Mode Message number (1-7)
+	 * @return The Mode_Msg<num> type (number) or 0 if unsuccessful
+	 * @see Base_Mode_Msg
+	 */
+	unsigned char Mode_Msg_type(unsigned short MM_num);
 }
 
 #endif
--- src/MIDItrackevt.cpp
+++ src/MIDItrackevt.cpp
@@ -2,8 +2,45 @@
 
 namespace MIDI
 {
-	unsigned char makeNote(MIDInote note, short octave)
+	unsigned char GPCtype(unsigned short GPC_num)
 	{
-		return (unsigned char)((int)note + 60 + 12 * octave);
+		if(GPC_num >= 1 && GPC_num <= 4)
+			return char((int)Base_GPC + GPC_num - 1);
+		else if(GPC_num >= 5 && GPC_num <= 8)
+			return char((int)Base_Upper_GPC + GPC_num - 1);
+		else
+			return (char)0;
+	}
+
+	unsigned char Controller_LSB_type(unsigned short cLSB_num)
+	{
+		if(cLSB_num >= 0 && cLSB_num <= 31)
+			return (char)int(Base_Controller_LSB) + cLSB_num;
+		else
+			return (char)0;
+	}
+
+	unsigned char Sound_Controller_type(unsigned short SC_num)
+	{
+		if(SC_num >= 1 && SC_num <= 10)
+			return (char)int(Base_Sound_Controller) + SC_num - 1;
+		else
+			return (char)0;
+	}
+
+	unsigned char Effects_type(unsigned short E_num)
+	{
+		if(E_num >= 1 && E_num <= 5)
+			return (char)int(Base_Effects) + E_num - 1;
+		else
+			return (char)0;
+	}
+
+	unsigned char Mode_Msg_type(unsigned short MM_num)
+	{
+		if(MM_num >= 1 && MM_num <= 7)
+			return (char)int(Base_Mode_Msg) + MM_num - 1;
+		else
+			return (char)0;
 	}
 }
